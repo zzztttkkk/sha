e12a321ccd26018ab94791cbb2bdcca35a9273b0
zzztttkkk <ztkisalreadytaken@gmail.com>
2020-08-04 11:27:41 +0800

    0.0

--- secret/init.go
+++ secret/init.go
@@ -1,14 +1,29 @@
 package secret
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/zzztttkkk/suna/config"
+	"io/ioutil"
+	"os"
 )
 
 var gSecretKey []byte
 
 func Init(conf *config.Type) {
 	gSecretKey = []byte(conf.Secret.Key)
+	if bytes.HasPrefix(gSecretKey, []byte("file://")) {
+		f, e := os.Open(string(gSecretKey[7:]))
+		if e != nil {
+			panic(e)
+		}
+		defer f.Close()
+		data, e := ioutil.ReadAll(f)
+		if e != nil {
+			panic(e)
+		}
+		gSecretKey = data
+	}
 
 	hashMethod := conf.Secret.HashAlgorithm
 	if len(hashMethod) < 1 {
