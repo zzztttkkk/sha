fd97c99cada577c1b13c50ff510fdf646426b372
zzztttkkk <ztkisalreadytaken@gmail.com>
2020-11-11 03:25:34 +0800

    00

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/zzztttkkk/suna
-
-go 1.15
-
-require github.com/valyala/bytebufferpool v1.0.0
--- onCopy.go
+++
@@ -1,6 +0,0 @@
-package suna
-
-type noCopy struct{} //nolint:unused
-
-func (*noCopy) Lock()   {}
-func (*noCopy) Unlock() {}
